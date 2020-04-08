1ba7a1ecc3f0869e3e2e87cd059bbb1fd0b32d7f
worrysprite <worrysprite@gmail.com>
2020-04-08 20:00:48 +0800

    添加memset所需的头文件

--- include/ws/core/Utils.h
+++ include/ws/core/Utils.h
@@ -1,6 +1,8 @@
 #ifndef __WS_UTILS_H__
 #define __WS_UTILS_H__
 
+#include <cstring>
+
 #define GET_FIELD_SIZE(from, to) ((uintptr_t)&to-(uintptr_t)&from+sizeof(to))
 #define ZERO_INIT(from, to) memset(&from, 0, GET_FIELD_SIZE(from, to))
 
