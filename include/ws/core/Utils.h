ef914a764128bd2b6223e46f92c7ec8175092d33
worrysprite <worrysprite@gmail.com>
2020-09-09 17:16:09 +0800

    给linux增加1、2字节整数的字面值

--- include/ws/core/Utils.h
+++ include/ws/core/Utils.h
@@ -165,6 +165,13 @@ namespace ws::core
 
 #if defined(__linux__) || defined(__linux) || defined(linux) || defined(__gnu_linux__)
 	bool createPidfile(const char* pidfile);
+
+	//方便使用1字节和2字节的整数字面值（MSVC支持而linux不支持）
+	constexpr int8_t operator "" i8(int i) { return (int8_t)i; }
+	constexpr uint8_t operator "" ui8(int i) { return (uint8_t)i; }
+	constexpr int16_t operator "" i16(int i) { return (int16_t)i; }
+	constexpr uint16_t operator "" ui16(int i) { return (uint16_t)i; }
+	
 #endif
 
 	// from boost hash_combine
